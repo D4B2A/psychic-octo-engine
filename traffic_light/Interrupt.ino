0e2e97165920c28abcdd5fa3ebd4ad56163290fe
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-03-20 22:33:47 +0100

    Update Interrupt.ino
    added loop and ISR

--- traffic_light/Interrupt.ino
+++ traffic_light/Interrupt.ino
@@ -1,1 +1,16 @@
-void 
+volatile bool buttonSchedule;
+
+void setup(){
+  pinMode(2,INPUT);
+  attachInterrupt(ISR,INT0,RISING); //INT0 => pin 2
+}
+
+void loop(){
+  if(buttonSchedule){
+    //do something
+  }
+}
+
+void ISR(){
+  buttonSchedule = true; //Button pressed Flag
+}
