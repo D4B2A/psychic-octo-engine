b78cb16d611201469182996fee558d8a607f08ea
D4B2A <128427718+D4B2A@users.noreply.github.com>
2023-03-21 17:04:03 +0100

    Update main.ino
    Added mask Class

--- traffic_light/main.ino
+++ traffic_light/main.ino
@@ -1,7 +1,9 @@
 volatile bool buttonSchedule = false;
 
 class trafficLight {
-public:
+
+    public:
+    
     byte redPin;
     byte orangePin;
     byte greenPin;
@@ -9,7 +11,7 @@ public:
     byte redGround;
     byte orangeGround;
     byte greenGround;
-
+    
     byte state;
 
     trafficLight(byte red, byte orange, byte green, byte redGnd, byte orangeGnd,byte greenGnd){
@@ -19,13 +21,37 @@ public:
 
         redGround = redGnd;
         orangeGround = orangeGnd;
-        greenGround = greenGnd
+        greenGround = greenGnd;
 
         state = 0;
     }
 
 };
 
+class mask{
+    private:
+    byte mask;
+    bool* port;
+    
+    getOnMask(){
+        return mask;
+    }
+    
+    getOfMask(){
+        return ~mask;
+    }
+    public:
+    
+    mask(bool dataport){
+        port = &dataport;
+    }
+    
+    applyMask(bool on){
+        if(on) *port |= getOnMask();
+        else *port &= getOfMask();
+        
+    }
+}
 
 
 void setup(){
